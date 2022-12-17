ec8ff4e2bf67540622cc4ff960a3ec0c9d78115f
happyntec <tec@happyn.cn>
2022-12-17 17:32:27 +0800

    #3

--- bin/uninstall.sh
+++ bin/uninstall.sh
@@ -5,11 +5,14 @@ rm -rf /Library/Extensions/tun.kext
 rm -rf /Library/Extensions/tap.kext
 
 # Unload an old extension (might fail).
-kextunload -b net.tunnelblick.tun
+kextunload -b net.tunnelblick.tun 2>/dev/null
+
+launchctl unload -w /Library/LaunchDaemons/net.happyn.plist 2>/dev/null
 
 
-rm -rf /Library/LaunchDaemons/happynet.plist
+rm -rf /Library/LaunchDaemons/net.happyn.plist
 rm -rf /Library/LaunchDaemons/net.tunnelblick.tap.plist
 rm -rf /Library/LaunchDaemons/net.tunnelblick.tun.plist
 rm -rf /Applications/happynet
 
+echo "happyn uninstall successful"
